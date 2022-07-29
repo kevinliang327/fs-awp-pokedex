08b2ee284f00d15db36b96689de7f42082f9f4b3
Kevin <admin@macbook-pro.mynetworksettings.com>
2022-07-29 01:16:35 -0400

    removed styled components

--- client/components/PokemonCard.js
+++ client/components/PokemonCard.js
@@ -1,10 +1,4 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-
-const Sprite = styled.img`
-  width: 5em;
-  height: 5em;
-`;
 
 export default class PokemonCard extends Component {
   state = {
@@ -43,10 +37,10 @@ export default class PokemonCard extends Component {
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
-        <div className="card">
+        <div className="pokemon-card card">
           <h5 className="card-header">{this.state.pokeIndex}</h5>
-          <Sprite
-            className="card-img-top rounded mx-auto mt-2"
+          <img
+            className="sprite card-img-top rounded mx-auto mt-2"
             src={this.state.imageUrl}
           />
           {this.state.error ? (
